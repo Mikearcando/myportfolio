d215c738bf583e1cca04a49b386fa9176f4a9ee5
mike aarnoutse <89360276+Mikearcando@users.noreply.github.com>
2025-11-05 10:00:25 +0100

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php $pageTitle = "Home | Portfolio van Mike Aarnoutse"; ?>
-<?php include 'header.php'; ?>
-        <section class="hero">
-            <h2>Welkom bij mijn portfolio</h2>
-            <p>Webdeveloper | Designer | Creator</p>
-        </section>
-<?php include 'footer.php'; ?>
